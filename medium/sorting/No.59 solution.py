74be9493945c1360811704be1e1e2b6392bac95c
Emily Tang <feixue1182@gmail.com>
2019-01-11 14:13:31 -0800

    Create No.59 solution.py

---
+++ medium/sorting/No.59 solution.py
@@ -0,0 +1,26 @@
+    def threeSumClosest(self, numbers, target):
+        # write your code here
+        length = len(numbers)
+        if length < 3:
+            return sum(numbers)
+        numbers.sort()
+        absolute = sys.maxsize
+        result = numbers[0]
+        for i in range(length-2):
+            left = i + 1
+            right = length-1
+            while left < length and left < right:
+                temp = numbers[i] + numbers[left] + numbers[right]
+                if temp == target:
+                    return target
+                elif absolute > abs(temp - target):
+                    print(result,absolute)
+                    absolute = abs(temp - target)
+                    result = temp
+                
+                elif temp < target:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return result
