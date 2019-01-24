f55a95a53dacd76becec02d38102065b3847062a
Emily Tang <feixue1182@gmail.com>
2019-01-23 23:07:56 -0800

    Update No.425 solution.py

--- medium/recursion/No.425 solution.py
+++ medium/recursion/No.425 solution.py
@@ -28,3 +28,21 @@
             # flatten list
                 result_2.append(res[i][j] + sstring[j])
         return result_2
+
+    
+    #solution 2: dfs
+    def letterCombinations(self, digits):
+        # write your code here
+        self.dic = {'2':['a','b','c'], '3':['d','e','f'], '4':['g','h','i'], '5':['j','k','l'], '6':['m','n','o'],'7':['p','q','r','s'],'8':['t','u','v'],'9':['w','x','y','z']}
+        self.result=[]
+        if len(digits) == 0:
+            return self.result
+        self.dfs(digits,'',0)
+        return self.result
+        
+    def dfs(self,digits,string,ind):
+        if ind == len(digits):
+            self.result.append(string)
+            return
+        for letter in self.dic[digits[ind]]:
+            self.dfs(digits,string+letter,ind+1)
