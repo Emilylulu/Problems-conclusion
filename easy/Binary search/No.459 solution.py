84a6a923bdb21faef0254706da41b049dc1f0cb4
Emily Tang <feixue1182@gmail.com>
2019-02-25 18:40:16 -0800

    Update No.459 solution.py

--- easy/Binary search/No.459 solution.py
+++ easy/Binary search/No.459 solution.py
@@ -16,39 +16,22 @@
             return ind
             
             
-            
     def closestNumber(self, A, target):
         # write your code here
-        # write binary search from scratch.
         left = 0
         right = len(A) - 1
-        if len(A) <= 1:
+        if len(A) <= 1 or A[0] >= target:
             return 0
-        while(left <= right):
+        if A[right] <= target:
+            return right
+        while(left < right):
             mid = int((left + right) / 2)
             if A[mid] == target:
                 return mid
             if A[mid] > target:
-                right = mid - 1
+                right = mid
             else:
                 left = mid + 1
-        # print(left,right)
-        if left == 0:
-            if abs(A[0] - target < abs(A[1] - target)):
-                return 0
-            else:
-                return 1
-        elif right == len(A) - 1:
-            if abs(A[right] - target) < abs(A[right - 1] - target):
-                return right
-            else:
-                return right - 1
-        else:
-        # can not import numpy so can not use minarg()
-            min_val = min(abs(A[left] - target), abs(A[left - 1] - target), abs(A[left + 1] - target))
-            if abs(A[left] - target) == min_val:
-                return left
-            if abs(A[left - 1] - target) == min_val:
-                return left - 1
-            else:
-                return left + 1
+        if target - A[left-1] <= A[left] - target:
+            return left - 1
+        return left
