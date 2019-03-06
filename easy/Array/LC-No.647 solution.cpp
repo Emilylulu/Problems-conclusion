a3b592ce0a3a16b67131c0cc3fb73564c93ccf26
Emily Tang <feixue1182@gmail.com>
2019-03-05 21:52:47 -0800

    Create LC-No.647 solution.cpp

---
+++ easy/Array/LC-No.647 solution.cpp
@@ -0,0 +1,18 @@
+  int findLengthOfLCIS(vector<int>& nums) {
+        int size = nums.size();
+        if(size <= 1) return size; // handle if size <= 1
+        int result = 0;
+        int i = 0;
+        int temp = 1;
+        
+        while(i<size - 1){
+            if(nums[i+1] <= nums[i]){ // do not forget increasing sequence can not be equal
+                if(temp > result) result = temp;
+                temp = 0;
+            }
+            temp += 1;
+            i++;
+        }
+        if(temp > result) result = temp; // handle if the last sequence is largest
+        return result;
+    }
